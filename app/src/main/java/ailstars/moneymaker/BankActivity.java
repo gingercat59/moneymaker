ecd72bf31ccbeec0ff4081b8f3589e3af42a643e
Ailstars <Ailstars@yandex.ru>
2017-10-20 12:54:45 +0500

    bank credit

--- app/src/main/java/ailstars/moneymaker/BankActivity.java
+++ app/src/main/java/ailstars/moneymaker/BankActivity.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.List;
 
 import ailstars.moneymaker.models.Bank;
+import ailstars.moneymaker.models.Player;
 
 public class BankActivity extends AppCompatActivity {
+    Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        player = Player.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank);
     }
@@ -20,6 +25,22 @@ public class BankActivity extends AppCompatActivity {
     public void bankbackward(View view) {
         Intent intent = new Intent(BankActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+    Spinner yearCREDIT = (Spinner) findViewById(R.id.yearCREDIT);
+    float t = (float) yearCREDIT.getSelectedItem();
+
+    EditText sumCREDIT = (EditText)findViewById(R.id.sumCREDIT);
+
+    int s = Integer.parseInt(sumCREDIT.getText().toString());
+
+    public void Click(View view){
+        if(s>5000 && t>0){
+            Bank.canTakeCredit(player.countMoney(),s,t); {
+
+                if(player.countMoney()> Bank.onePayment(s,t)){
+                }
+            }
+        }
     }
 
 
--- app/src/main/java/ailstars/moneymaker/models/Bank.java
+++ app/src/main/java/ailstars/moneymaker/models/Bank.java
@@ -1,8 +1,11 @@
 package ailstars.moneymaker.models;
 
+import android.content.Intent;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,25 +18,41 @@ import ailstars.moneymaker.R;
 
 public class Bank {
     private List<Account> accountList = new ArrayList<>();
-
+    private float sumDebt;
+    private float sumCredit;
+    private float timeCredit;
 
     //проверить можно ли открыть депозит
-    private boolean proverkaDepozit (Float countmoney) {
-        if (0 < countmoney) {
+    private boolean proverkaDepozit (Float countmoney,Float addMoney) {
+        if (sumDebt > addMoney && 0 < countmoney) {
             return true;
         }
         return false;
     }
+    //1 платёж
+    public static float onePayment (float sumCredit, float timeCredit){
+        float n = sumCredit / timeCredit;
+        float pers=0;
+        for(double i=timeCredit; i>0; i--){
+            pers += (float) (pers + Math.pow(((100 + getPercent(Account.Types.CREDIT)) / 100), i) * n);
+        }
+        return ((sumCredit+pers)/timeCredit)/52;
+    }
     //проверка для кредита
-
+    public static boolean canTakeCredit (Float countMoney, float sumCredit, float timeCredit){
+        if(countMoney > onePayment(0,0)){
+            return true;
+        }
+        return  false;
+    }
     //если можно добавить новый пункт в список
-
     public void createAccount(Account.Types type, Float money, Integer time) {
         accountList.add(Account.getInstance(type, getPercent(type), money, time));
     }
 
     private void addAccount(Account account) {
         accountList.add(account);
+
     }
     //вернуть весь список
 
@@ -44,6 +63,7 @@ public class Bank {
         }
         return Float.valueOf(10);
     }
+
 
 
 
